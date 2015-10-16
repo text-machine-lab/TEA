64b813c0afee9fc107581c634ba341038a6018ff
kevin wacome <kdubya12345@gmail.com>
2015-10-16 13:43:13 -0400

    added jar dependencies for ixa-pipes-1.1.0, pre-processing

---
+++ code/notes/NewsReader/tokenization/Sentenizer.java
@@ -0,0 +1,16 @@
+
+package pre_processing.sentenization;
+
+import eus.ixa.ixa.pipe.seg.SentenceSegmenter;
+
+class Sentenizer {
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
+
---
+++ code/notes/NewsReader/tokenization/runner.sh
@@ -0,0 +1,6 @@
+
+SENTENIZER_DEPENDENCIES=":ixa-pipes-1.1.0-jars/ixa-pipe-tok-1.8.2.jar"
+
+javac -cp $SENTENIZER_DEPENDENCIES Sentenizer.java -d .
+
+
