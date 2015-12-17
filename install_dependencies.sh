695620fcb81c223eea4d7fb14137f3b33328cc3b
ktwaco <kdubya12345@gmail.com>
2015-12-16 21:58:32 -0500

    fixed install script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,9 +2,21 @@
 # unpack dependencies
 tar -xvf NewsReader.tar
 
+# installing srl dependencies
+wget https://mate-tools.googlecode.com/files/srl-4.3.tgz
+
+tar -zxvf srl-4.3.tgz srl-20130917/lib/liblinear-1.51-with-deps.jar
+tar -zxvf srl-4.3.tgz srl-20130917/lib/seg.jar
+tar -zxvf srl-4.3.tgz srl-20130917/srl.jar
+
+mvn install:install-file -Dfile=srl-20130917/lib/liblinear-1.51-with-deps.jar -DgroupId=local -DartifactId=liblinear -Dversion=1.51 -Dpackaging=jar
+mvn install:install-file -Dfile=srl-20130917/srl.jar -DgroupId=local -DartifactId=srl -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=srl-20130917/lib/seg.jar -DgroupId=local -DartifactId=seg -Dversion=1.0 -Dpackaging=jar
+
 # extract srl tar ball
 tar -xvf NewsReader/ixa-pipes-1.1.0/ixa-pipe-srl.tar -C NewsReader/ixa-pipes-1.1.0/
 
+# JAVA 1.7 or higher worked fine. ensure maven is using right version.
 mvn clean install -f NewsReader/ixa-pipes-1.1.0/ixa-pipe-srl/IXA-EHU-srl/pom.xml
 
 wget http://mate-tools.googlecode.com/files/CoNLL2009-ST-English-ALL.anna-3.3.parser.model --directory-prefix=NewsReader/ixa-pipes-1.1.0/ixa-pipe-srl/IXA-EHU-srl/target/models/eng/
@@ -17,4 +29,6 @@ tar -zxf PredicateMatrix.srl-module.tar.gz -C NewsReader/ixa-pipes-1.1.0/ixa-pip
 rm -f PredicateMatrix.srl-module.tar.gz
 
 mv NewsReader code/notes/
+
+
 
