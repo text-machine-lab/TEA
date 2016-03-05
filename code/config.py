a0837bfe02c381a7f0d3973444ebc71f8304618c
ktwaco <kdubya12345@gmail.com>
2016-03-05 09:55:50 -0500

    added config file

---
+++ code/config.py
@@ -0,0 +1,26 @@
+import os
+
+TEA_HOME_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+
+# inspired by cliner...
+def env_paths():
+
+    paths = open(os.path.join(TEA_HOME_DIR, "config.txt"), "rb").read()
+    paths = paths.strip('\n')
+    paths = [line.split() for line in paths.split('\n')]
+
+    for line in paths:
+        path = None if line[1] == 'None' else line[1]
+
+        if line[0] == "PY4J_DIR_PATH":
+            if path is not None and os.path.isdir(path) is False:
+                raise Exception("ERROR: PY4J_DIR_PATH directory is invalid")
+
+    env_paths = {line[0]:(None if line[1] == 'None' else line[1]) for line in paths}
+
+    return env_paths
+
+if __name__ == "__main__":
+
+    print env_paths()
+
