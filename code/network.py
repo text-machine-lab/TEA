269724b2b8567073568db5309dceef541e7339df
c-cooper <cotiger3@gmail.com>
2016-04-30 14:12:50 -0400

    added preprocessing for labels so that they can be fed into the network

--- code/network.py
+++ code/network.py
@@ -88,6 +88,28 @@ def _get_token_id_subpaths(note):
 
     return left_paths, right_paths
 
+def _pre_process_labels(labels):
+    '''convert tlink labels to integers so they can be processed accordingly'''
+
+    processed_labels = []
+    for label in labels:
+        if label == "SIMULTANEOUS":
+            processed_labels.append(1)
+        elif label == "BEFORE":
+            processed_labels.append(2)
+        elif label == "AFTER":
+            processed_labels.append(3)
+        elif label == "IS_INCLUDED":
+            processed_labels.append(4)
+        elif label == "BEGUN_BY":
+            processed_labels.append(5)
+        elif label == "ENDED_BY":
+            processed_labels.append(6)
+        else:  # label for pairs which are not linked
+            processed_labels.append(0)
+
+    return processed_labels
+
 if __name__ == "__main__":
     test = NNModel()
     tmp_note = TimeNote("APW19980418.0210.tml.TE3input", "APW19980418.0210.tml")
@@ -96,12 +118,13 @@ if __name__ == "__main__":
     test.train([tmp_note])
 
     labels = tmp_note.get_tlink_labels()
+    labels = _pre_process_labels(labels)
+    labels = to_categorical(labels,7)
     print len(labels)
+    print labels
     input1 = np.random.random((len(labels),300))
     input2 = np.random.random((len(labels),300))
     # labels = np.random.randint(7, size=(10000,1))
-    labels = to_categorical(labels,7)
-    print labels
     test.classifier.fit([input1,input2], labels, nb_epoch=100)
     print test.classifier.predict_classes([input1,input2])
     pass
