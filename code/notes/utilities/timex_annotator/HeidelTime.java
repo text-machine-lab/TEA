9ba03448588f3fce887e974ca7f6da8eb32a1e73
ktwaco <kdubya12345@gmail.com>
2016-05-18 19:06:01 -0400

    moved where heideltime interface resides

---
+++ code/notes/utilities/timex_annotator/HeidelTime.java
@@ -0,0 +1,53 @@
+import java.util.Date;
+import java.util.Calendar;
+import de.unihd.dbs.heideltime.standalone.*;
+import de.unihd.dbs.uima.annotator.heideltime.resources.Language;
+import de.unihd.dbs.heideltime.standalone.exceptions.DocumentCreationTimeMissingException;
+
+public class HeidelTime {
+
+    HeidelTimeStandalone heidelTime = new HeidelTimeStandalone(Language.ENGLISH,
+                                                               DocumentType.COLLOQUIAL,
+                                                               OutputType.TIMEML,
+                                                              // FIX THIS!
+                                                              "conf/config.props",
+                                                               POSTagger.TREETAGGER, true);
+
+    int[] _month_consts = {0, Calendar.JANUARY, Calendar.FEBRUARY, Calendar.MARCH, Calendar.APRIL, Calendar.MAY,
+                           Calendar.JUNE, Calendar.JULY, Calendar.AUGUST, Calendar.SEPTEMBER, Calendar.OCTOBER,
+                           Calendar.NOVEMBER, Calendar.DECEMBER};
+
+    public String process(String document, int year, int month, int day) {
+
+        //WARNING: I don't check for value correctness.
+        int month_const = _month_consts[month];
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH, month_const);
+        cal.set(Calendar.DAY_OF_MONTH, day);
+
+        try {
+            return heidelTime.process(document, cal.getTime());
+        } catch(DocumentCreationTimeMissingException e) {
+            System.out.println("ERROR: Missing doc time.");
+            System.exit(-1);
+        }
+
+        return "NULL";
+
+    }
+
+    public static void  main(String[] args) {
+
+        HeidelTime test = new HeidelTime();
+
+        test.process("He ran yesterday", 1993, 4, 26);
+
+        return;
+
+    }
+
+
+}
+
--- dependencies/HeidelTime/code/test_compile.sh
+++
@@ -1,4 +0,0 @@
-
-javac -cp ".:target/de.unihd.dbs.heideltime.standalone.jar" HeidelTime.java
-java -cp ".:lib/*:target/de.unihd.dbs.heideltime.standalone.jar" HeidelTime
-
