9c8543b60f117f8db1d6e2eeb3b165e8fd9f83fa
ktwaco <kdubya12345@gmail.com>
2016-05-18 17:38:59 -0400

    added heidel time and some test code

---
+++ dependencies/HeidelTime/install_treetagger.sh
@@ -0,0 +1,10 @@
+
+mkdir treetagger
+cd treetagger
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/tree-tagger-linux-3.2.tar.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/tagger-scripts.tar.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/install-tagger.sh
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/english-par-linux-3.2-utf8.bin.gz
+bash install-tagger.sh
+cd ..
+
